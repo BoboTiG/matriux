0e2e31706b5fea862610c90971ee1b9382bcaccf
BoboTiG <bobotig@gmail.com>
2012-10-30 19:19:53 +0100

    First commit

---
+++ Matriux Disk  Installer/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Installation script for Matriux Disk Installer.
+#
+
+dest="/pt/matriux/MID"
+icon="/pt/matriux/icons/48x48"
+
+if [ $(id -ru) -ne 0 ]; then
+	echo "[!] You need to have root rights !"
+	exit 0
+fi
+[ ! -d ${dest} ] && mkdir -p ${dest}
+[ ! -d ${icon} ] && mkdir -p ${icon}
+install mid CHANGELOG CREDITS DEBUG LICENSE ${dest}
+install icon.png ${icon}
+chmod +x ${dest}/mid
+install mid.desktop /usr/share/applications
+echo " Installation done."
+exit 0
